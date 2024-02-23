a72df570ad4e5765b1aa54fe91ef96c75e614202
aM-aShakur <28875216+aM-aShakur@users.noreply.github.com>
2024-02-23 11:53:46 -0500

    Update

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-"use strict";
+// create a container that holds the button 
+// and text input from user
+var container = document.createElement('container');
+container.id = 'mainContainer';
+container.setAttribute('style', 'border: solid black 2px;');
+var textBox = document.createElement('p');
+textBox.id = 'text';
+textBox.innerHTML = ("Pick a number between:\n1 and 50\nFor the grid size.");
+textBox.setAttribute('style', 'border: solid black 2px');
+container.appendChild(textBox);
+// input 
+var input = document.createElement('input');
+input.id = 'inputElement';
+// input.setAttribute('style', 'border: solid 2px red;')
+container.appendChild(input);
 // create a button that prompts the user
 // prompt determines the size of the grid
-const button = document.createElement('button');
+var button = document.createElement('button');
 // get prompt from user
 // grid size can be anywhere between 1 - 50
 function howMany() {
-    const result = prompt("Pick a number between:\n1 and 50\nFor the grid size.");
-    const number = Number(result);
+    var _a;
+    // input value
+    var inputValue = (_a = document.getElementById('inputElement')) === null || _a === void 0 ? void 0 : _a.value;
+    var number = Number(inputValue);
     if (number > 0 && number < 51) {
-        console.log(number);
-        return number;
+        // fill the div with the user's number
+        textBox.textContent = number.toString();
     }
     if (number <= 0 || number > 50) {
-        return alert('Error:\nPick a number between\n1 and 50');
+        textBox.textContent = ('Error:\nPick a number between 1 and 50\nTry again!');
     }
 }
+var mainGrid = document.createElement('div').classList.add('mainGrid');
+function createDiv(num) {
+    num = Number(textBox.textContent);
+    for (var i = 0; i < (num * num); i++) {
+        var grid = document.createElement("div".concat(i));
+        grid.classList.add('grid');
+        mainGrid.appendChild(grid);
+    }
+}
+createDiv(25);
 // add the func howMany to button
 // add the button to the dom
 button.addEventListener('click', howMany);
-button.textContent = ("Click to begin");
-document.body.appendChild(button);
-// const mainGrid: HTMLElement = document.createElement('mainGrid').classList.add('container')
-/** function createDiv (num : number) {
-    for (let i = 0; i < num; i++) {
-        const newDiv: HTMLElement = document.createElement(`div${i}`)
-        newDiv.classList.add('container')
-        mainGrid.appendChild(newDiv)
-    }
-    
-}
-***/
+button.textContent = ("Click Me");
+// add the elements to the document body
+document.body.append(container);
+document.body.append(button);
+document.body.append(textBox);
+document.body.append(mainGrid);
--- script.ts
+++ script.ts
@@ -1,43 +1,72 @@
+// create a container that holds the button 
+// and text input from user
+const container: any  = document.createElement('container');
+container.id = 'mainContainer';
+container.setAttribute('style', 'border: solid black 2px;')
+
+const textBox : HTMLElement = document.createElement('p');
+textBox.id = 'text';
+textBox.innerHTML = ("Pick a number between:\n1 and 50\nFor the grid size.");
+textBox.setAttribute('style', 'border: solid black 2px')
+container.appendChild(textBox);
+
+// input 
+const input: HTMLInputElement = document.createElement('input');
+input.id = 'inputElement';
+// input.setAttribute('style', 'border: solid 2px red;')
+container.appendChild(input)
+
 // create a button that prompts the user
 // prompt determines the size of the grid
-
-const button: HTMLElement = document.createElement('button')
+const button: HTMLElement = document.createElement('button');
 
 // get prompt from user
 // grid size can be anywhere between 1 - 50
-function howMany() {
+function howMany() : number {
+    // input value
+    const inputValue = (document.getElementById('inputElement') as HTMLInputElement | null)?.value;
+    const number: number = Number(inputValue)
 
-    
-    const result: string | null  = prompt("Pick a number between:\n1 and 50\nFor the grid size.")
-    const number: number = Number(result)
-    
     if (number > 0 && number < 51 ) {
-        console.log(number);
-        return number;
+        // fill the div with the user's number
+        textBox.textContent = number.toString();
     } 
 
     if (number <= 0 || number > 50) {
-    return alert('Error:\nPick a number between\n1 and 50')
+    textBox.textContent = ('Error:\nPick a number between 1 and 50\nTry again!');
     }
-   
+    console.log(number)
+   return number
 }
 
-// add the func howMany to button
-// add the button to the dom
-button.addEventListener('click', howMany);
-button.textContent = ("Click to begin"); 
-document.body.appendChild(button)
+const mainGrid: HTMLElement | any = document.createElement('div').classList.add('mainGrid')
 
-// const mainGrid: HTMLElement = document.createElement('mainGrid').classList.add('container')
-
-/** function createDiv (num : number) {
-    for (let i = 0; i < num; i++) {
-        const newDiv: HTMLElement = document.createElement(`div${i}`)
-        newDiv.classList.add('container')
-        mainGrid.appendChild(newDiv)
+function createDiv (num : number) {
+    num = howMany()
+    for (let i = 0; i < (num * num); i++) {
+        const grid: HTMLElement = document.createElement(`div${i}`)
+        grid.classList.add('grid')
+        mainGrid.appendChild(grid)
+        return mainGrid;
     } 
     
 }
-***/
+
+createDiv(howMany())
+// add the func howMany to button
+// add the button to the dom
+button.addEventListener('click', howMany);
+button.textContent = ("Click Me"); 
 
 
+
+
+
+
+
+
+// add the elements to the document body
+document.body.append(container);
+document.body.append(button);
+document.body.append(textBox);
+document.body.append(mainGrid)
